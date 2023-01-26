56726450849ca036b9002f2a4b1486ddf8384a36
markischengg <markischeng28@uchicago.edu>
2023-01-26 03:11:41 +0000

    omnicalc square a number

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
 
+  get("square/new", { :controller => "application", :action => "blank_square_form"})
+
+  get("square_root/new", { :controller => "application", :action => ""})
+
+  get("payment/new", { :controller => "application", :action => ""})
+
+  get("random/new", { :controller => "application", :action => ""})
+
 end
